a33493f1f3f276f5cd6088fc405f7669c7d07ccd
khoza-tb <tsepokhoza266@gmail.com>
2026-08-20 02:22:56 +0200

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

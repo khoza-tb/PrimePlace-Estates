f7cac6f8b880549c58a15224a87388b46afeae18
khoza-tb <tsepokhoza266@gmail.com>
2026-08-27 13:58:53 +0200

    updating my api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,3 @@
-import express from "express";
-import { test } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/", test);
-
-export default router;
+export const test = (req, res) => {
+  res.json({ message: "Hello World" });
+};

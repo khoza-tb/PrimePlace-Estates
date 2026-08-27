583865d7c7107d05b8e0bd74182559b5b90b78ac
khoza-tb <tsepokhoza266@gmail.com>
2026-08-27 13:47:23 +0200

    create a text api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,29 @@
-import expresss from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("connected to MongoDB");
-}).catch((err) => {
-  console.log(err);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "PrimePlaceEstate API is running" });
 });
 
-const app = expresss();
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+  console.log("🚀 Server is running on port 3000");
 });

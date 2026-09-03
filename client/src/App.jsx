a59e653943e3c4a9f055d7f0a4db4113fb47ef88
khoza-tb <tsepokhoza266@gmail.com>
2026-09-03 16:37:42 +0200

    updating header and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   )
--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "primeplaceestate.firebaseapp.com",
   projectId: "primeplaceestate",
   storageBucket: "primeplaceestate.firebasestorage.app",
   messagingSenderId: "908408857144",
-  appId: "1:908408857144:web:1fef26b5801257198a7ccd"
+  appId: "1:908408857144:web:1fef26b5801257198a7ccd",
 };
 
-// Initialize Firebase
- export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
